011e9917851e62c9aceaf6cb36e1a501701eaaa6
Yang <qiaoyanandyang@gmail.com>
2021-10-07 15:39:58 +0800

    Adding Dummy Data & More Content

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,11 @@
+import QuoteList from "../components/quotes/QuoteList";
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "learning" },
+  { id: "q2", author: "Esther", text: "hello" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
